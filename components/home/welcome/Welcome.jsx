aa18c1aace9f372ea190c552332dbd9f07ebc66d
Edward Joseph Pegollo <pegolloedwardjoseph@gmail.com>
2023-07-07 20:02:07 +0800

    Initial commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
+import { icons, SIZE, SIZES } from "../../../constants";
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Edward!</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job.</Text>
+      </View>
+
+      {/* Search bar */}
+      <View style={styles.searchContainer}>
+        {/* Search Input */}
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        {/* Search Button */}
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          ></Image>
+        </TouchableOpacity>
+      </View>
+
+      {/* Job Types */}
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
